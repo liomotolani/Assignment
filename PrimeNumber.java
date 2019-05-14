6824af381649b5cdfc3811a7d0ffe382827cf937
unknown <ligaliomotolani@gmail.com>
2019-05-14 23:26:35 +0100

    Correction on my assignment

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -5,8 +5,15 @@ public class PrimeNumber{
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter any number: ");
 		int number = input.nextInt();
-		if(number % 2 == 1 || number == 2){
-		System.out.println(number+" is a prime number");
+		int count = 0;
+			for(int i = 2; i<= number/2; i++){
+		if(number %  i == 0){
+		count++;
+		break;
+			}
+		}
+		if(count == 0 && number !=1){
+			System.out.println(number+" is a prime number");
 		}
 	}
 }
